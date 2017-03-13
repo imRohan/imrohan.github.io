8a5b51e13d31965529e0cb7f4f689b79daadfd89
imRohan <rlikhite@gmail.com>
2017-03-13 14:05:02 -0400

    made amsterdam a bit more cooler

--- amsterdam/js/timer.js
+++ amsterdam/js/timer.js
@@ -2,10 +2,10 @@ var Timer = (function () {
 
   var startTimer = function () {
     _timeLeftContainer = $('.js-timeleft-container')
-    _tripStartDate = moment("2017-05-08 19:20:00");
-    _todaysDate = moment();
-    _difference = _todaysDate.to(_tripStartDate, true);
-    _timeLeftContainer.html(_difference)
+    setInterval(function(){
+      _countdown = countdown(new Date(2017,05,08)).toString()
+      _timeLeftContainer.html(_countdown)
+    },1000)
   };
   
   return {
