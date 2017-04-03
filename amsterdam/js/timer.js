ec1e5da17aaaefcf35af5d34fdba090b3bb92af8
imRohan <rlikhite@gmail.com>
2017-04-03 16:16:49 -0400

    fixed date for Amsterdam lol

--- amsterdam/js/timer.js
+++ amsterdam/js/timer.js
@@ -3,7 +3,7 @@ var Timer = (function () {
   var startTimer = function () {
     _timeLeftContainer = $('.js-timeleft-container')
     setInterval(function(){
-      _countdown = countdown(new Date(2017,05,08)).toString()
+      _countdown = countdown(new Date(2017,04,08)).toString()
       _timeLeftContainer.html(_countdown)
     },1000)
   };
