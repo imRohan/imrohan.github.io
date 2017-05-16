7c5d4c2fbbed894715b23875d8773bf0151278b8
imRohan <rlikhite@gmail.com>
2017-05-16 15:11:50 -0400

    added component for title

--- amsterdam/js/vueLogic.js
+++ amsterdam/js/vueLogic.js
@@ -1,6 +1,28 @@
 var vueMain = (function () {
-
+  
   var initMainVue = function () {
+    Vue.component('hero-title', {
+      props: ['mode'],
+      computed: {
+        getTitle: function() {
+          _this = this;
+          return _this.mode.title;
+        },
+        getSubtitle: function() {
+          _this = this;
+          return _this.mode.subtitle;
+        },
+      },
+      template: `<div class='text-title'>
+                  <div class='text-superscript'>
+                    {{getSubtitle}}
+                  </div>
+                  <div class='text-bold'>
+                    {{getTitle}}
+                  </div>
+                </div>`,
+    });
+
     new Vue({
       el: '.main',
       data: {
@@ -10,7 +32,7 @@ var vueMain = (function () {
           subtitle: 'May 8th - 14th',
         }
       }
-    })
+    });
   };
   
   return {
@@ -18,3 +40,4 @@ var vueMain = (function () {
   };
 
 })();
+
